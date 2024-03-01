3237c5499d5c6369a5f96f7e3f4da28eadb95980
MatiasSolisBello <matias.solis.bello@gmail.com>
2024-03-01 17:34:55 -0300

    Poo II

--- 01_basic/04_poo.py
+++ 01_basic/04_poo.py
@@ -1,15 +1,24 @@
+from datetime import date
+
 class Car:
     # Constructor
-    def __init__(self, color, max_speed, brand):
+    def __init__(self, color, max_speed, brand, date_fabrication):
         self.color = color
         self.max_speed = max_speed
         self.brand = brand
+        self.date_fabrication = date_fabrication
 
-    def speed_up(self): # metodo
-        print(f'we are accelerating with a max. of {self.max_speed}')
+    def get_data(self):
+        print(f'{self.color}, {self.brand}, {self.max_speed}, {self.date_fabrication}')
+
+    # calcular la edad en base a su fecha de fabricación
+    def get_age(self):
+        year_fabrication = self.date_fabrication.year
+        today = date.today().year
+        return print(today - year_fabrication)
 # End of class
 
-
-myCar = Car('blue', 200, "Toyota") # objeto o instancia de la clase auto
-print(f'{myCar.color}, {myCar.brand}, {myCar.max_speed}')
-myCar.speed_up()    # retorna lo indicado en el metodo
+# objeto o instancia de la clase auto
+myCar = Car('blue', 200, "Toyota", date(2000, 2, 2))
+myCar.get_data()
+myCar.get_age()
